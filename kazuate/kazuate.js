e2a37f7a4a598a264bf441ee71def42f58cadf7a
G184312021 <218431@st.takushoku-u.ac.jp>
2022-05-26 15:20:38 +0900

    a

--- kazuate/kazuate.js
+++ kazuate/kazuate.js
@@ -3,27 +3,37 @@ console.log('答え: ' + kotae);
 
 let kaisu = 0;
 
-hantei();
+let l = document.querySelector('button#print');
+l.addEventListener('click', hantei);
 
 function hantei() {
-    let yoso = 4;
+    let yoso = document.querySelector('input[name="seisuu"]');
+    let yoso1 = yoso.value;
+    yoso1 = Math.floor(yoso1);
     kaisu = kaisu + 1;
-    console.log(kaisu + "回目の予想: " + yoso);
-    if (yoso === kotae) {
-        console.log("正解です。おめでとう！");
-    } 
-    else if (yoso !== kotae && yoso > kotae) {
-        console.log("まちがい。答えはもっと小さいですよ");
-    }
-    else {
-        console.log("まちがい。答えはもっと大きいですよ");
-    }
-    if (kaisu > 3) {
-        console.log("答えは " + kotae + " でした．すでにゲームは終わっています");
-    }
+    let dp = document.querySelector('p#yoso');
+    dp.textContent = (kaisu + "回目の予想: " + yoso1);
+    let d = document.querySelector('p#answer');
     
+    
+    if (kaisu > 3) {
+       
+        d.textContent = ("答えは " + kotae + " でした．すでにゲームは終わっています");
+        
+    } else {
+        if (yoso1 === kotae) {
+            d.textContent = ("正解です。おめでとう！");
+        } 
+        else if (yoso1 !== kotae && yoso1 > kotae) {
+            d.textContent = ("まちがい。答えはもっと小さいですよ");
+        }
+        else {
+            d.textContent = ("まちがい。答えはもっと大きいですよ");
+        }
+    }   
 }
 
-let p = document.querySelector('p#result');
-p.textContent = kotae;
+
+
+
 
